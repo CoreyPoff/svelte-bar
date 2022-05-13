0c116104a7e754fc7049438c2cd21dd8640d5e5c
Corey Poff <mrcpoff@gmail.com>
2022-05-12 20:10:11 -0400

    First commit

---
+++ src/lib/Recipe.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let name:string;
+  export let directions:string;
+</script>
+
+<h2>{name}</h2>
+<p>{directions}</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Recipe from '../lib/Recipe.svelte';
+</script>
+
+<h1>Dashed ( ಠ ͜ʖ ರೃ) Proper</h1>
+
+
+<Recipe name="Sveltini" directions="Stir with ice for 15 seconds. Pour into chilled glass." />
+<Recipe name="Svelte Fizz" directions="Add egg white. Shake vigorously." />
+
+
